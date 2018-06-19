162c45ca2dcb416784062bc8250727af72326ebf
psitronic <spintronic@tuta.io>
2018-06-19 11:28:58 +0200

    Add offset for data points

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
 const margin = {top: 30, right: 20, bottom: 30, left: 50},
 	width = 640 - margin.left - margin.right,
-	height =280 - margin.top - margin.bottom;
+	height = 400 - margin.top - margin.bottom;
 
 const parseYear = d3.timeParse("%Y");
 const parseTime = d3.timeParse("%M:%S");
@@ -22,22 +22,26 @@ d3.json(dataUrl, (error, dataset) => {
 		};
 	});
 
-	// console.log(data);
+	// console.log(data[0].Year.setFullYear(data[0].Year.getFullYear() - 10));
 
 
-	const minYear = d3.min(data, (d) => d.Year);
-	const maxYear = d3.max(data, (d) => d.Year);
+	const minYear = parseYear(d3.min(data, (d) => d.Year).getFullYear() - 1);
+	const maxYear = parseYear(d3.max(data, (d) => d.Year).getFullYear() + 1);
 	const minTime = d3.min(data, (d) => d.Time);
 	const maxTime = d3.max(data, (d) => d.Time);
 
+	// console.log(parseYear(minYear.getFullYear() - 1));
 
 	var xScale = d3.scaleTime()
 					.domain([minYear, maxYear])
-					.range([0, width]);
+					.range([0, width])
+					.nice();
 
     var yScale = d3.scaleTime()
                     .domain([maxTime, minTime])
-                    .range([height, 0]);
+                    .range([height, 0])
+    				.nice();
+
 
 
 	var svg = d3.select("#graph")
@@ -49,8 +53,7 @@ d3.json(dataUrl, (error, dataset) => {
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	var xAxis = d3.axisBottom(xScale);
-	var yAxis = d3.axisLeft(yScale)
-					.tickFormat(d3.timeFormat("%M:%S"));
+	var yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%M:%S")).ticks(d3.timeSecond.every(30));;
 
 	svg.append("g")
 		.attr("id", "x-axis")
@@ -61,7 +64,6 @@ d3.json(dataUrl, (error, dataset) => {
 	svg.append("g")
 		.attr("id", "y-axis")
 		.attr("class", "axis")
-		// .attr("transform", "translate( " + (margin.left + margin.right) + ",0)")
 		.call(yAxis);
 
 
