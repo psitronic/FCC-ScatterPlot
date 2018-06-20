c18a811defa27985b15887389614ed774a2c1d91
psitronic <spintronic@tuta.io>
2018-06-20 08:57:24 +0200

    Add tooltip and some properties

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
 const margin = {top: 30, right: 20, bottom: 30, left: 50},
-	width = 640 - margin.left - margin.right,
-	height = 400 - margin.top - margin.bottom;
+	width = 1000 - margin.left - margin.right,
+	height = 600 - margin.top - margin.bottom;
 
 const parseYear = d3.timeParse("%Y");
 const parseTime = d3.timeParse("%M:%S");
@@ -25,8 +25,8 @@ d3.json(dataUrl, (error, dataset) => {
 	// console.log(data[0].Year.setFullYear(data[0].Year.getFullYear() - 10));
 
 
-	const minYear = parseYear(d3.min(data, (d) => d.Year).getFullYear() - 1);
-	const maxYear = parseYear(d3.max(data, (d) => d.Year).getFullYear() + 1);
+	const minYear = d3.min(data, (d) => d.Year);
+	const maxYear = d3.max(data, (d) => d.Year);
 	const minTime = d3.min(data, (d) => d.Time);
 	const maxTime = d3.max(data, (d) => d.Time);
 
@@ -43,17 +43,42 @@ d3.json(dataUrl, (error, dataset) => {
     				.nice();
 
 
+	var div = d3.select("body").append("div")
+		.attr("class", "tooltip")
+		.attr("id", "tooltip")
+		.style("opacity", 0);
 
 	var svg = d3.select("#graph")
 		.append("svg")
-		.attr("class", "graph")
+		// .attr("class", "graph")
 		.attr("width", width + margin.left + margin.right)
 		.attr("height", height + margin.top + margin.bottom)
 		.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	var xAxis = d3.axisBottom(xScale);
-	var yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%M:%S")).ticks(d3.timeSecond.every(30));;
+	var yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%M:%S"));
+
+	svg.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("x", - height/2)
+		.attr("y", 30)
+		.text("GDP [Million USD]")
+		.attr("class", "axisTitle");
+
+	svg.append("text")
+		.attr("x", (width)/2)
+		.attr("y", height + 50)
+		.text("Time [Year]")
+		.attr("class", "axisTitle");
+
+	svg.append("text")
+		.attr("id", "title")
+		.attr("x", (width / 2))
+		.attr("y", 0 - (margin.top / 2))
+		.attr("text-anchor", "middle")
+		.text("Doping in Professional Bicycle Racing")
+		.attr("class", "title");
 
 	svg.append("g")
 		.attr("id", "x-axis")
@@ -73,17 +98,32 @@ d3.json(dataUrl, (error, dataset) => {
 		.attr("class", "dot")
 		.attr("cx", (d) => xScale(d.Year))
 		.attr("cy", (d) => yScale(d.Time))
-		.attr("r", 5)
-		.attr("opacity", 0.5)
-		.attr("color", "black")
-		.attr("fill", (d) => {
+		.attr("r", 7)
+		.attr("data-xvalue", (d) => d.Year)
+		.attr("data-yvalue", (d) => d.Time)
+		.style("fill", (d) => {
 			if (d.Doping === "") {
 				return "lightgreen";
 			} else {
 				return "steelblue";
 			}
+		})
+		.on("mouseover", function(d) {
+			div.transition()
+			.duration(200)
+			.style("opacity", .9);
+			div
+			.html(d.Name + ": " + d.Nationality + "<br/>" + "Year: " + d.Year.getFullYear()
+				+ ", Time: " + d3.timeFormat("%M:%S")(d.Time) + "<br/>" + d.Doping)
+        	.attr('data-year', d.Year)
+			.style("left", (d3.event.pageX + 23) + "px")
+			.style("top", (d3.event.pageY - 23) + "px");
+			})
+		.on("mouseout", function(d) {
+			div.transition()
+			.duration(500)
+			.style("opacity", 0);
 		});
-
 
 	// console.log(data);
 });
